866f6e5285c5cf2af605bc7d390ed81e0e58270a
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-11-14 14:21:18 +0100

    attach the database

--- restaurant/db_connect.php
+++ restaurant/db_connect.php
@@ -1,1 +1,12 @@
 <?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "Restaurant";
+
+$connect = mysqli_connect($host, $user, $pass, $dbname);
+
+if (!$connect) {
+  die("Connection failed: " . mysqli_connect_error());
+}
